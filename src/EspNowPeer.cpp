5ecabccac4c97a2a7414a73386f302433efc4d32
marcelopi <marcelo-pimentel@hotmail.com>
2025-05-19 12:49:22 -0300

    Uso da nova biblioteca MqttEspnowPeer2Peer nos devices

--- src/EspNowPeer.cpp
+++ src/EspNowPeer.cpp
@@ -217,7 +217,7 @@ void EspNowPeer::onReceive(const uint8_t* mac, const uint8_t* data, int len) {
     }
     // Subscreve PING para todos os filhos e envia WQ   QW
     if (destination == instance->localName && action == "PING") {
-        instance->publishENow(destination, source, "PING", "PONG");
+        instance->publishENow(destination, source, "PONG", "PONG");
         Serial.println("🔁 Respondido PING com PONG");
         return;
     }
